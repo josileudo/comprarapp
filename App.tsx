f192b307b0ec1728f8092ca1470a31219b65bdc7
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2025-05-27 10:18:52 -0300

    :sparkles: feat: implements image

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import {Home} from "./src/app/home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={{flex: 1, padding: 10}}>
+        <Home />
+      </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
