218db76522ac9fb323a8e9f842374a329dd1a3e3
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2025-05-27 11:21:42 -0300

    :sparkles: feat: Create input component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#2C4681',
+        width: '100%',
+        borderRadius: 8,
+        height: 48,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    label: {
+        color: "#fff",
+        fontSize: 14,
+        fontWeight: 600,
+    }
+})
