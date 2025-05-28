fbd75327e2d25d3977e28fa14bbed23c57ebe7a8
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2025-05-28 12:04:22 -0300

    :sparkles: feat: Implements persistence data with AsyncStorage
    This commit introduces the implementation of data persistence using AsyncStorage to store and retrieve items.
    
    The changes include:
    
    - Installed `@react-native-async-storage/async-storage` for local data storage.
    - Created `src/storage/itemsStorage.ts` to manage item storage operations (get, getByStatus, add).
    - Modified `src/app/home/index.tsx` to:
      - Fetch items from AsyncStorage on component mount.
      - Add new items to AsyncStorage.
      - Clear all items.
      - Update the item list when a new item is added.
    - Added state management for description input in `src/app/home/index.tsx`.
    - Implemented `handleAddItem` function to add items.
    - Implemented `handleDeleteAll` function to clear all items.

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,4 +1,11 @@
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
@@ -6,52 +13,84 @@ import { Filter } from "@/components/Filter";
 import { FilterStatus } from "@/types/FilterStatus";
 import { Item } from "@/components/Item";
 import { ShieldAlert } from "lucide-react-native";
+import { useEffect, useState } from "react";
+import { itemsStorage, ItemStorage } from "@/storage/itemsStorage";
 
 export function Home() {
+  const [filter, setFilter] = useState<FilterStatus>(FilterStatus.DONE);
+  const [description, setDescription] = useState<string>("");
+  const [items, setItems] = useState<ItemStorage[]>([]);
   const FILTER_STATUS: FilterStatus[] = [
     FilterStatus.PENDING,
     FilterStatus.DONE,
   ];
 
-  const ITEM_MOCK = [
-    {
-      id: "1",
-      description: "Bolacha recheada",
-      status: FilterStatus.DONE,
-    },
-    {
-      id: "2",
-      description: "Macarräo",
+  const getItems = async () => {
+    try {
+      const response = await itemsStorage.get();
+      setItems(response);
+    } catch (e) {
+      console.log(e);
+      Alert.alert("Error", "Não foi possível renderizar seus itens");
+    }
+  };
+
+  const handleAddItem = async () => {
+    if (!description.trim())
+      Alert.alert("Atenção", "Adicionar um item no campo");
+
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
       status: FilterStatus.PENDING,
-    },
-    {
-      id: "3",
-      description: "Feijäo",
-      status: FilterStatus.PENDING,
-    },
-  ];
+    };
+
+    await itemsStorage.add(newItem);
+    await getItems();
+    setDescription("");
+  };
+
+  const handleDeleteAll = () => {
+    setItems([]);
+  };
+
+  useEffect(() => {
+    getItems();
+  }, []);
 
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
 
       <View style={styles.form}>
-        <Input placeholder={"O que deseja comprar?"} />
-        <Button label="Adicionar" />
+        <Input
+          placeholder={"O que deseja comprar?"}
+          value={description}
+          onChangeText={setDescription}
+        />
+        <Button label="Adicionar" onPress={handleAddItem} />
       </View>
 
       <View style={styles.content}>
         <View style={styles.header}>
           {FILTER_STATUS.map((status) => (
-            <Filter key={status} status={status} isActive />
+            <Filter
+              key={status}
+              status={status}
+              isActive={status === filter}
+              onPress={() => setFilter(status)}
+            />
           ))}
-          <TouchableOpacity style={styles.clearButton}>
+          <TouchableOpacity
+            style={styles.clearButton}
+            onPress={handleDeleteAll}
+          >
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
 
         <FlatList
-          data={ITEM_MOCK}
+          data={items}
           keyExtractor={(item) => item.id}
           showsVerticalScrollIndicator={false}
           renderItem={({ item }) => (
---
+++ src/storage/itemsStorage.ts
@@ -0,0 +1,48 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { FilterStatus } from "@/types/FilterStatus";
+
+const ITEMS_STORAGE_KEY = "@COMPRAR:ITEMS";
+const STATUS_STORAGE_KEY = "@COMPRAR:STATUS";
+
+export type ItemStorage = {
+  id: string;
+  status: FilterStatus;
+  description: string;
+};
+
+async function get(): Promise<ItemStorage[]> {
+  try {
+    const storage = await AsyncStorage.getItem(ITEMS_STORAGE_KEY);
+    return storage ? JSON.parse(storage) : [];
+  } catch (e) {
+    throw new Error(`ITEMS_GET ${e}`);
+  }
+}
+
+async function getByStatus(status: FilterStatus): Promise<ItemStorage[]> {
+  const items = await get();
+  return items.filter((item) => item.status === status);
+}
+
+async function save(items: ItemStorage[]): Promise<void> {
+  try {
+    await AsyncStorage.setItem(ITEMS_STORAGE_KEY, JSON.stringify(items));
+  } catch (e) {
+    throw new Error(`ITEMS_SAVE ${e}`);
+  }
+}
+
+async function add(item: ItemStorage): Promise<ItemStorage[]> {
+  const items = await get();
+  const updatedItems = [...items, item];
+
+  await save(updatedItems);
+
+  return updatedItems;
+}
+
+export const itemsStorage = {
+  get,
+  getByStatus,
+  add,
+};
